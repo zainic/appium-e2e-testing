62262d3f2a5781a4e667dca9202a7f5db1bfdab2
Dzaky Tamir <dzakytamir160702@gmail.com>
2024-05-11 18:52:01 +0700

    Fixed base project

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,5 @@
+import { join } from 'path'
+
 export const config = {
     //
     // ====================
@@ -110,7 +112,7 @@ export const config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['appium'],
+    services: [],
 
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
